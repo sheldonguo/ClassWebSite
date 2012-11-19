c37e19e2c795862896a841f2dcf526e8eff1cd8a
allenjin <allen3jin@163.com>
2012-11-19 20:06:23 +0800

    谢了导航栏

--- html/index.php
+++ html/index.php
@@ -1,4 +1,27 @@
 <html>
-	<head></head>
-	<body></body>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="../css/default.css" />
+	</head>
+	<body>
+		<div id="cw-header">
+			<div id="cw-top-bar">
+				<div id="cw-logo">
+					<a>logo</a>
+				</div>
+				<ul id="cw-navigation">
+					<li><a href="#">首页</a></li>
+					<li><a href="#">班风</a></li>
+					<li><a href="#">博文</a></li>
+					<li><a href="#">资源</a></li>
+				</ul>
+				<ul id="cw-login">
+					<li id="nav-login"><a href="#">登陆</a></li>
+					<li id="nav-register"><a href="#">注册</a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="cw-content"></div>
+		<div id="cw-footer"></div>
+	</body>
 </html>
